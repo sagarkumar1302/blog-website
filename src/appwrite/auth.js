ca6c84f9573ca430dd1785cb830b293e952f1513
Sagar <imsgr10@gmail.com>
2025-04-03 21:47:53 +0530

    implement AuthService for user authentication and configuration setup

---
+++ src/appwrite/auth.js
@@ -0,0 +1,75 @@
+import config from "../config/config";
+import { Client, Account, Databases, Storage, ID } from "appwrite";
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(config.appWrite)
+      .setProject(config.appWritePorjectId);
+    this.account = new Account(this.client);
+    // this.databases = new Databases(this.client, config.appWriteDatabaseId);
+    // this.storage = new Storage(this.client, config.appWriteBucketId);
+  }
+  async createAccount(email, password, name) {
+    try {
+      const response = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (response) {
+        return this.login(email, password);
+      }
+      else {
+        throw new Error("Account creation failed");
+      }
+    } catch (error) {
+      console.error("Error creating account:", error);
+      throw error;
+    }
+  }
+  async login(email, password) {
+    try {
+      const response = await this.account.createEmailPasswordSession(email, password);
+      if (response) {
+        return response;
+      } else {
+        throw new Error("Login failed");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
+      throw error;
+    }
+  }
+  async logout() {
+    try {
+      const response = await this.account.deleteSession("current");
+      if (response) {
+        return response;
+      } else {
+        throw new Error("Logout failed");
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      const response = await this.account.get();
+      if (response) {
+        return response;
+      } else {
+        throw new Error("Failed to get current user");
+      }
+    } catch (error) {
+      console.error("Error getting current user:", error);
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+export default authService;
